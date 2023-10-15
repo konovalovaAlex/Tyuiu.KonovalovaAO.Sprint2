c14cddeec8180a33cce8fff24d467faa2ca17986
konovalovaAlex <144355131+konovalovaAlex@users.noreply.github.com>
2023-10-15 18:10:54 +0500

    Task5

---
+++ Tyuiu.KonovalovaAO.Sprint2.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KonovalovaAO.Sprint2.Task5.V4.Lib;
+namespace Tyuiu.KonovalovaAO.Sprint2.Task5.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("Пики", ds.FindCardSuit(1));
+            Assert.AreEqual("Трефы", ds.FindCardSuit(2));
+            Assert.AreEqual("Бубны", ds.FindCardSuit(3));
+            Assert.AreEqual("Червы", ds.FindCardSuit(4));
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindCardSuit(0);
+            });
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                ds.FindCardSuit(5);
+            });
+        }
+    }
+}
---
+++ Tyuiu.KonovalovaAO.Sprint2.Task5.V4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KonovalovaAO.Sprint2.Task5.V4.Lib;
+namespace Tyuiu.KonovalovaAO.Sprint2.Task5.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила Коновалова А. О. /СМАРТб-23-1 ";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Тернарный оператор                                               *");
+            Console.WriteLine("* Задание #5                                                             *");
+            Console.WriteLine("* Вариант #4                                                             *");
+            Console.WriteLine("* выполнила: Коновалова Александра Олеговна | СМАРТб-23-1                *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет       *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                             *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int value;
+            Console.WriteLine("Введите номер масти от 1 до 4: ");
+            value = Convert.ToInt32(Console.ReadLine());
+            string res;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Резльтат                                                                *");
+            Console.WriteLine("***************************************************************************");
+
+            if (value > 4 || value < 1)
+            {
+                res = "Такой масти нет!";
+            }
+            else
+            {
+                res = "Ваша масть : " + ds.FindCardSuit(value);
+            }
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
